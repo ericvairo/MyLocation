e561a65b20acc66a423968d8d2200e5af1e51dce
ericvairo <ericvairo@gmail.com>
2015-07-27 11:49:52 -0300

    Atualização de localização

--- app/src/main/java/com/example/eric/mylocation/MainActivity.java
+++ app/src/main/java/com/example/eric/mylocation/MainActivity.java
@@ -3,19 +3,29 @@ package com.example.eric.mylocation;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
+
 
 
 public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
 
     private TextView tvCoordinate;
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
 
 
     @Override
@@ -27,6 +37,22 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
         callConnection();
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(mGoogleApiClient !=null && mGoogleApiClient.isConnected()){
+            startLocationUpdate();
+        }
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if(mGoogleApiClient != null){
+            stopLocationUpdate();
+        }
+    }
+
     private synchronized void callConnection(){
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -36,6 +62,24 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
                 .build();
         mGoogleApiClient.connect();
 
+    }
+
+    private void initLocationRequest(){
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(2000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void startLocationUpdate(){
+        initLocationRequest();
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, MainActivity
+                .this);
+    }
+
+    private void stopLocationUpdate(){
+
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,MainActivity.this);
     }
 
 
@@ -55,6 +99,7 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
         }else{
             tvCoordinate.setText("nao sei | nao sei ");
         }
+        startLocationUpdate();
 
 
     }
@@ -67,5 +112,18 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Log.i("LOG","onConnectionFailed("+connectionResult+")");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        tvCoordinate.setText(Html.fromHtml("Latitude: "+location.getLatitude()+"<br/>"+
+                        "Longitude: "+location.getLongitude()+"<br/>"+
+                        "Bearing: "+location.getBearing()+"<br/>"+
+                        "Altitude: "+location.getAltitude()+"<br/>"+
+                        "Speed: "+location.getSpeed()+"<br/>"+
+                        "Provider: "+location.getProvider()+"<br/>"+
+                        "Accuracy: "+location.getAccuracy()+"<br/>"+
+                        "Date: "+ DateFormat.getTimeInstance().format(new Date())+"<br/>"));
+
     }
 }
